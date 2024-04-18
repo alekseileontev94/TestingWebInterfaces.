4e829a9230d6034f0bdcd308b0534d4eed3f281a
Aleksei Leontev <aleksei_leontev94@mail.ru>
2024-04-18 19:35:52 +0500

    chore: inital commit

---
+++ src/test/java/ru/netology/TestingWebInterfaces.java
@@ -0,0 +1,50 @@
+package ru.netology;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WebInterfaceTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldSuccessCardOrder() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Алексей Леонтьев");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+73211540382");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualTextElements = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualText = actualTextElements.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        assertTrue(actualTextElements.isDisplayed());
+    }
+}
