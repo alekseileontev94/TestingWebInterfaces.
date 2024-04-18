2acc39779d939f0a4996885942d441a4a3c0774d
Aleksei Leontev <aleksei_leontev94@mail.ru>
2024-04-18 19:43:14 +0500

    TestingWebInterfaces

--- src/test/java/ru/netology/TestingWebInterfaces.java
+++ src/test/java/ru/netology/TestingWebInterfaces.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class WebInterfaceTest {
+public class TestingWebInterfaces {
     private WebDriver driver;
 
     @BeforeAll
